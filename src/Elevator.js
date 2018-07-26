cc2446c577e85b73b5d29c2401b6cb0752fb8b31
Adam Horowitz <ahorowi4@gmail.com>
2018-07-26 19:12:20 -0400

    select is working

--- src/Elevator.js
+++ src/Elevator.js
@@ -15,15 +15,28 @@ class Elevator extends Component {
     console.log("Initialized Elevator");
   };
 
-  moveThroughFloors = () => {
-    return new Promise(resolve => {
+  delay = ms => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  };
+
+  move = async () => {
+    this.moving = true;
+    console.log("Elevator is moving");
+
+    while (
+      this.ascendingFloors.length !== 0 ||
+      this.descendingFloors.length !== 0
+    ) {
       while (this.ascendingFloors.length !== 0) {
         console.log("current floor " + this.currentFloor);
         this.currentDirection = 1;
         if (this.currentFloor === this.ascendingDestination) {
-          console.log("%c Reached floor " + this.currentFloor, "color: green;");
-          this.ascendingFloors = this.ascendingFloors.slice(1)
+          console.log("ascendingFloors: " + this.ascendingFloors);
+          console.log("%cReached floor " + this.currentFloor, "color: green;");
+          this.removeFromAscending();
+          console.log("ascendingFloors after: " + this.ascendingFloors);
         } else {
+          await this.delay(3000);
           this.currentFloor++;
         }
       }
@@ -31,21 +44,16 @@ class Elevator extends Component {
         console.log("current floor " + this.currentFloor);
         this.currentDirection = -1;
         if (this.currentFloor === this.descendingDestination) {
-          console.log("%c Reached floor " + this.currentFloor, "color: green;");
-          this.descendingFloors.pop();
+          console.log("%cReached floor " + this.currentFloor, "color: green;");
+          this.removeFromDescending();
         } else {
+          await this.delay(3000);
           this.currentFloor--;
         }
       }
-      resolve();
-    });
-  };
+    }
 
-  move = async () => {
-    this.moving = true;
-
-    await this.moveThroughFloors();
-
+    console.log("Elevator has stopped");
     this.moving = false;
   };
 
@@ -56,6 +64,11 @@ class Elevator extends Component {
     this.ascendingDestination = noDups[0];
   };
 
+  removeFromAscending = () => {
+    this.ascendingFloors.shift();
+    this.ascendingDestination = this.ascendingFloors[0];
+  }
+
   addToDescending = floor => {
     const newDescending = [...this.descendingFloors, floor];
     const noDups = [...new Set(newDescending)].sort();
@@ -63,8 +76,13 @@ class Elevator extends Component {
     this.descendingDestination = noDups[noDups.length - 1];
   };
 
+  removeFromDescending = () => {
+    this.descendingFloors.pop();
+    this.descendingDestination = this.descendingFloors[this.descendingFloors.length - 1];
+  }
+
   select = floor => {
-    console.log("%c Selecting floor " + floor, "color: blue;");
+    console.log("%cSelecting floor " + floor, "color: blue;");
     if (floor !== this.currentFloor) {
       if (floor < this.currentFloor) {
         this.addToDescending(floor);
@@ -74,13 +92,20 @@ class Elevator extends Component {
       if (!this.moving) {
         this.move();
       }
+    } else {
+      if (this.moving) {
+        console.log("adding");
+        if (this.currentDirection === 1) {
+          this.addToDescending(floor);
+        } else if (this.currentDirection === -1) {
+          this.addToAscending(floor);
+        }
+      }
     }
   };
 
   render() {
-    return (
-      <SelectFloor handleOnSelect={this.select} />
-    );
+    return <SelectFloor handleOnSelect={this.select} />;
   }
 }
 
